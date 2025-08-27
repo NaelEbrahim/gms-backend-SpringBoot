74fece44b0325bb09cb0cb5190c27dbfac953ab6
NAEL PC <nael@example.com>
2025-08-27 12:54:08 +0300

    In this push: Modify : add Coach Image to Class Response

--- src/main/java/com/graduation/GMS/DTO/Response/UserResponse.java
+++ src/main/java/com/graduation/GMS/DTO/Response/UserResponse.java
@@ -21,7 +21,14 @@ public class UserResponse {
 
     private String email;
 
+    private String profileImagePath;
 
+    public UserResponse(Integer id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 
     public static UserResponse mapToUserResponse(User user) {
         return new UserResponse(
